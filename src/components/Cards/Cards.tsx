6a8300a28f1d001503b29e9805f56007d2dbad09
Sinisa Zlatkovic <simke.vgv@gmail.com>
2021-05-16 14:35:53 +0200

    sd

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getData } from "../../reducers/FetchApiReducer/ApiActions";
 import { RootStore } from "../../Store";
-import AddNewImg from "../AddMewImg/AddNewImg";
+import AddNewImg from "../AddNewImg/AddNewImg";
 import ElInsideCard from "../ElInsideCard/ElInsideCard";
 import "./Card.css";
 
